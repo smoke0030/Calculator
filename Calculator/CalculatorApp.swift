fb9ab062be5584d08c08d3aac6cd6d2a8d90fea9
Sergey Vidishev <ft.priest@gmail.com>
2024-08-08 15:55:06 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Сергей on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
